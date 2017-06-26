423a40184e6c0d1c9a34a2ef665a82a88dea7d0e
TomerAdmon <tomer.a@quali.com>
2017-06-26 12:04:10 +0300

    redirect 7.1.0 version to the old 7.1 dev guide

--- js/version-redirect.js
+++ js/version-redirect.js
@@ -1,6 +1,13 @@
 function versionChange()	 {
 	var targetVersion = document.getElementById("version").value;
-	var currentVersion = window.location.href.match(/\d\.\d\.\d/i)[0];
-	document.getElementById("version").value = targetVersion
-	window.location.href = window.location.href.replace(currentVersion,targetVersion);
+	if (targetVersion == "7.1.0")
+	{
+		window.location = "https://qualisystems.github.io/devguide_7/";
+	}	
+	else
+	{
+		var currentVersion = window.location.href.match(/\d\.\d\.\d/i)[0];
+		document.getElementById("version").value = targetVersion
+		window.location.href = window.location.href.replace(currentVersion,targetVersion);
+	}
 };
